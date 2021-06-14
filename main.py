d78f739ea2b25d77719d0da4e170e2b3fc5e4855
Jorge L. Vivas C <slacksky@users.noreply.github.com>
2021-06-14 13:46:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+""" Programa ventas 
+    Programa para el manejo de ventas por mes
+    incorpora al modulo ventas.py
+    Oscar Franco-Bedoya
+    Junio 2-2021 """
+
+#---------------- Zona librerias------------
+import ventas as vt
+
+#======================================================================
+#          E S P A C I O    D E    T R A B A J O     A L U M N O
+# ====================================================================
+
+#----------Definición de Funciones (Dividir)------------
+
+#======================================================================
+#   Algoritmo principal Punto de entrada a la aplicación (Conquistar)
+# =====================================================================
---
+++ ventas.py
@@ -0,0 +1,38 @@
+""" Modulo Module ventas
+    Funciones para el manejo de ventas mensuales
+    con matrices
+    Oscar Franco-Bedoya
+    Mayo 10-2021 """
+# Definición de Funciones
+#======================================================================
+#          E S P A C I O    D E    T R A B A J O     A L U M N O
+# ====================================================================
+def leer_numero_empleados():
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def leer_ventas_empleados_mes():
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def ordenar_vendeores_por_ventas():
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def calcular_cinco_vendedores():
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def calcula_mes_mas_ventas():
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def greficar_ventas_por_mes():
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
